aeef6bf2a92c2f6222504b40c77665e058a58dd3
JavierDiosesU51L <119764554+JavierDiosesU51L@users.noreply.github.com>
2025-11-10 17:50:48 -0500

    feat: main.cpp incrementado con rotaciones y recorrido inorden

--- main.cpp
+++ main.cpp
@@ -9,6 +9,28 @@ struct NodoTreap {
     NodoTreap *izquierda, *derecha;
 };
 
+// Rotación a la derecha
+NodoTreap* rotarDerecha(NodoTreap* y) {
+    NodoTreap* x = y->izquierda;
+    NodoTreap* T2 = x->derecha;
+
+    x->derecha = y;
+    y->izquierda = T2;
+
+    return x;
+}
+
+// Rotación a la izquierda
+NodoTreap* rotarIzquierda(NodoTreap* x) {
+    NodoTreap* y = x->derecha;
+    NodoTreap* T2 = y->izquierda;
+
+    y->izquierda = x;
+    x->derecha = T2;
+
+    return y;
+}
+
 // Crear un nuevo nodo
 NodoTreap* nuevoNodo(int clave) {
     NodoTreap* temp = new NodoTreap;
@@ -16,4 +38,18 @@ NodoTreap* nuevoNodo(int clave) {
     temp->prioridad = rand() % 100;
     temp->izquierda = temp->derecha = nullptr;
     return temp;
+}
+
+// Recorrido en orden del Treap
+void recorridoEnOrden(NodoTreap* raiz) {
+    if (raiz) {
+        recorridoEnOrden(raiz->izquierda);
+        cout << "Clave: " << raiz->clave << " | Prioridad: " << raiz->prioridad;
+        if (raiz->izquierda)
+            cout << " | Hijo izquierdo: " << raiz->izquierda->clave;
+        if (raiz->derecha)
+            cout << " | Hijo derecho: " << raiz->derecha->clave;
+        cout << endl;
+        recorridoEnOrden(raiz->derecha);
+    }
 }
